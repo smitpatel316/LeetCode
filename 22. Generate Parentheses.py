723ef6c26fbca1935d50a4f91a5e296da61c6f23
Smit Patel <smitpatel316@gmail.com>
2020-02-18 16:49:58 -0500

    Runtime: 28 ms. Memory: 13 MB.

---
+++ 22. Generate Parentheses.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        perms = []
+        self.backtrack(n, "", 0, 0, perms)
+        return perms
+
+    def backtrack(self, n, str, left, right, perms):
+        if len(str) == 2*n:
+            perms.append(str)
+        else:
+            if left < n:
+                self.backtrack(n, str+'(', left+1, right, perms)
+            if right < left:
+                self.backtrack(n, str+')', left, right+1, perms)
+
+if __name__ == "__main__":
+    print(Solution().generateParenthesis(3))
+
