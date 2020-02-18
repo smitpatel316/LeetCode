031c8d38e2f07a24e2ed2637254a4756ad951223
Smit Patel <smitpatel316@gmail.com>
2020-02-18 16:11:45 -0500

    Runtime: 28 ms. Memory: 12.8 MB.

---
+++ 94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if root is None:
+            return []
+        else:
+            return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
