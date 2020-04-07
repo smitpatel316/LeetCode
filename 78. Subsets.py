f641c94c76f00e53d596293836ccd85ee4a15714
Smit Patel <smitpatel316@gmail.com>
2020-04-07 04:30:01 -0400

    Runtime: 28ms. Memory Usage: 13.8 MB.

---
+++ 78. Subsets.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        output = [[]]
+
+        for num in nums:
+            output += [curr + [num] for curr in output]
+        return output
