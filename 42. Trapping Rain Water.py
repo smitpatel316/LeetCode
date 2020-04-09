0220aedf9b4bd3ed66ee5bd3974897e3e324cfbf
Smit Patel <smitpatel316@gmail.com>
2020-04-09 05:53:36 -0400

    Runtime: 48 ms. Memory Usage: 14.7 MB.

---
+++ 42. Trapping Rain Water.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left, left_max = 0, 0
+        right, right_max = len(height) - 1, 0
+        total_water = 0
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    total_water += left_max - height[left]
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    total_water += right_max - height[right]
+                right -= 1
+        return total_water
+
+
+if __name__ == "__main__":
+    print(Solution().trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
