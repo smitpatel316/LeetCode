011c256f74acca89da763fe82ab795b8d1e66ece
Smit Patel <smitpatel316@gmail.com>
2020-04-09 05:15:23 -0400

    Runtime: 20 ms. Memory Usage: 14 MB.

---
+++ 191. Number of 1 Bits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        hamming_weight = 0
+        while n != 0:
+            hamming_weight += 1
+            n &= (n - 1)
+        return hamming_weight
+
+
+if __name__ == "__main__":
+    print(Solution().hammingWeight(0o0000000000000000000000000001011))
