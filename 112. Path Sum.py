c847739af94024ff3793dba10f07345a2cc46fc4
Smit Patel <smitpatel316@gmail.com>
2020-08-20 11:30:05 -0400

    Runtime: 44 ms. Memory Usage: 15.7 MB.

---
+++ 112. Path Sum.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        if root.val == sum and root.left is None and root.right is None:
+            return True
+        sum -= root.val
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
