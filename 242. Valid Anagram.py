5225f8ad474b271bbb4bcf513ac572bec865f3a3
Smit Patel <smitpatel316@gmail.com>
2020-09-08 02:42:07 -0400

    Slower but reliable solution

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,3 +1,4 @@
+from collections import Counter
 class Solution(object):
     def isAnagram(self, s, t):
         """
@@ -5,4 +6,15 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        return sorted(s) == sorted(t)
+        occur = dict(Counter(s))
+        for c in t:
+            if c not in occur or occur[c] == 0:
+                return False
+            else:
+                occur[c] -= 1
+
+        for char in occur:
+            if occur[char] != 0:
+                return False
+
+        return True
