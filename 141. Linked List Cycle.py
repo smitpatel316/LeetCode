b91df323167eaec6520dc0850a9d0dfe1d5d6682
Smit Patel <smitpatel316@gmail.com>
2020-09-15 11:26:49 -0400

    optimized

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -1,20 +1,20 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        seen = []
-        curr = head
-        if head == None:
+        if not head or not head.next:
             return False
-        while curr != None:
-            if curr in seen:
+
+        curr = head
+        ahead = head.next
+        while curr and ahead and ahead.next:
+            if curr == ahead:
                 return True
-            else:
-                seen.append(curr)
-                curr = curr.next
+            curr = curr.next
+            ahead = ahead.next.next
         return False
