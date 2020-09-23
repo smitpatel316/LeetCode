c9c4aa4780ff6372ddceff3fcde8eb049d7a5ba7
Smit Patel <smitpatel316@gmail.com>
2020-09-23 18:49:28 -0400

    Runtime: 32 ms. Memory Usage: 13.8 MB.

---
+++ 17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,32 @@
+from typing import List
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        num_to_code = {
+            2: ["a", "b", "c"],
+            3: ["d", "e", "f"],
+            4: ["g", "h", "i"],
+            5: ["j", "k", "l"],
+            6: ["m", "n", "o"],
+            7: ["p", "q", "r", "s"],
+            8: ["t", "u", "v"],
+            9: ["w", "x", "y", "z"],
+        }
+        output = []
+        for digit in digits:
+            if not output:
+                output = num_to_code[int(digit)]
+            else:
+                new_output = []
+                for i in range(len(output)):
+                    for c in num_to_code[int(digit)]:
+                        new_output.append(output[i] + c)
+                output = new_output
+        return output
+
+
+if __name__ == "__main__":
+    print(Solution().letterCombinations("23"))
