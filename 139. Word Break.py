95d82e08e0d1371e88f12f84837706b4a8f8f086
Smit Patel <smitpatel316@gmail.com>
2020-10-09 17:46:54 -0400

    Runtime: 40 ms. Memory Usage: 14.3 MB.

--- 139. Word Break.py
+++ 139. Word Break.py
@@ -1,9 +1,21 @@
-from typing import List
-
-
 class Solution:
+    def __init__(self):
+        self.dp = {}
+
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        ok = [True]
-        for i in range(1, len(s) + 1):
-            ok.append(any(ok[j] and s[j:i] in wordDict for j in range(i)))
-        return ok[-1]
+        return self.dfs(s, set(wordDict))
+
+    def dfs(self, word, word_dict):
+        if not word:
+            return True
+        if not word_dict and word:
+            return False
+        if word in self.dp:
+            return self.dp[word]
+        for i in range(len(word)):
+            if word[:i + 1] in word_dict and self.dfs(word[i + 1:], word_dict):
+                self.dp[word] = True
+                return True
+        self.dp[word] = False
+        return False
+
