dbb8fb3c47323b896279bf3cef3e9ab3c112e699
Smit Patel <smitpatel316@gmail.com>
2020-10-11 01:05:08 -0400

    Bucket Sort

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -6,3 +6,11 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = Counter(nums)
         return [elem for (elem, count) in counter.most_common(k)]
+
+    # Bucket Sort
+    def topKFrequent(self, nums, k):
+        bucket = [[] for _ in range(len(nums) + 1)]
+        Count = Counter(nums).items()
+        for num, freq in Count: bucket[freq].append(num)
+        flat_list = [item for sublist in bucket for item in sublist]
+        return flat_list[::-1][:k]
