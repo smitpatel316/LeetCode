38c07afede5e4a3f9a0f475932f7a31e62a35588
Smit Patel <smitpatel316@gmail.com>
2020-12-27 23:21:41 -0500

    Runtime: 244 ms. Memory Usage: 17.8 MB.

---
+++ 1010. Pairs of Songs With Total Durations Divisible by 60.py
@@ -0,0 +1,15 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def numPairsDivisibleBy60(self, time: List[int]) -> int:
+        remainders = collections.defaultdict(int)
+        ret = 0
+        for t in time:
+            if t % 60 == 0:
+                ret += remainders[0]
+            else:
+                ret += remainders[60 - t % 60]
+            remainders[t % 60] += 1
+        return ret
